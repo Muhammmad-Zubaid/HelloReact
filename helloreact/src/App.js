ee3b0885fa02a77d7a5acf611fee868d5e1a9f14
Muhammad-Zubaid <muhammad.zubaid@hotmail.com>
2019-09-28 17:09:06 +0500

    State Practice.

--- helloreact/src/App.js
+++ helloreact/src/App.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [Counter,setCounter] = useState(10)
+  let [name , setName] = useState("")
+  let [obj,setObj] = useState({name : "" , age : 10})
+
+  const UpdateCounter = () => {
+    setCounter(Counter +1 )
+  }
+
+  const UpdateName = () => {
+    setName(name  = "Muhammad Zubaid" )
+  }
+
+  const UpdateObject = () => {
+    const newObj = {...obj};
+    newObj.name ="Muhammad Asad";
+    newObj.age = 60 ;
+    newObj.Class = "Intermediate"
+
+//    setObj(newObj);
+    setObj({
+      name : "Muhammad Zuabid",
+      age : 50,
+      Class : "Matric"
+    });
+
+ 
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+    {Counter}
+    <br/>
+    {name}
+    <br/>
+    {obj.name} - {obj.age} - {obj.Class}
+    <br/>
+    <button onClick={() => {setCounter(Counter +1 )}}>update</button>
+    <button onClick={UpdateCounter} >update 2</button>
+    <button onClick={UpdateName} >update Name</button>
+    <button onClick={UpdateObject} >update oBJECT</button>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +56,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
